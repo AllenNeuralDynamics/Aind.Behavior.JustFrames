31b20d00645b07221c86b9b590f798fa12e5c3f6
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-12-02 20:31:14 -0800

    Update launcher dependencies

--- src/aind_behavior_just_frames/launcher.py
+++ src/aind_behavior_just_frames/launcher.py
@@ -5,7 +5,6 @@ from aind_behavior_services.session import AindBehaviorSessionModel
 from clabe import resource_monitor
 from clabe.apps import (
     AindBehaviorServicesBonsaiApp,
-    BonsaiAppSettings,
 )
 from clabe.data_transfer import robocopy
 from clabe.launcher import Launcher, LauncherCliArgs
@@ -31,12 +30,15 @@ def experiment(launcher: Launcher) -> None:
     session = picker.pick_session(AindBehaviorSessionModel)
     launcher.register_session(session)
     rig = picker.pick_rig(AindJustFramesRig)
-    bonsai_app = AindBehaviorServicesBonsaiApp(BonsaiAppSettings(workflow=Path(r"./src/main.bonsai")))
-    bonsai_app.add_app_settings(launcher, rig=rig, session=session)
-    bonsai_app.get_result(allow_stderr=True)
+    bonsai_app = AindBehaviorServicesBonsaiApp(
+        workflow=Path(r"./src/main.bonsai"),
+        rig=rig,
+        session=session,
+    )
+    bonsai_app.run()
 
     launcher.copy_logs()
-    robocopy.RobocopyService(source=launcher.session_directory, settings=robocopy.RobocopySettings())
+    robocopy.RobocopyService(source=launcher.session_directory, settings=robocopy.RobocopySettings()).transfer()
     return
 
 
