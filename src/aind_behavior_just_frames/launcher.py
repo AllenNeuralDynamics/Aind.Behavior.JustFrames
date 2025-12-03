a38fd7d774a1b196d6832c147caf5b7a32da9a25
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-12-03 08:29:22 -0800

    Add qc to launcher

--- src/aind_behavior_just_frames/launcher.py
+++ src/aind_behavior_just_frames/launcher.py
@@ -11,6 +11,7 @@ from clabe.launcher import Launcher, LauncherCliArgs
 from clabe.pickers import DefaultBehaviorPicker, DefaultBehaviorPickerSettings
 from pydantic_settings import CliApp
 
+from aind_behavior_just_frames import data_contract
 from aind_behavior_just_frames.rig import AindJustFramesRig
 
 logger = logging.getLogger(__name__)
@@ -36,6 +37,24 @@ def experiment(launcher: Launcher) -> None:
         session=session,
     )
     bonsai_app.run()
+
+    # Run data qc
+    if picker.ui_helper.prompt_yes_no_question("Would you like to generate a qc report?"):
+        try:
+            import webbrowser
+
+            from contraqctor.qc.reporters import HtmlReporter
+
+            from .data_qc.data_qc import make_qc_runner
+
+            _dataset = data_contract.dataset(launcher.session_directory)
+            runner = make_qc_runner(_dataset)
+            qc_path = launcher.session_directory / "Behavior" / "Logs" / "qc_report.html"
+            reporter = HtmlReporter(output_path=qc_path)
+            runner.run_all_with_progress(reporter=reporter)
+            webbrowser.open(qc_path.as_uri(), new=2)
+        except Exception as e:
+            logger.error("Failed to run data QC: %s", e)
 
     launcher.copy_logs()
     robocopy.RobocopyService(source=launcher.session_directory, settings=robocopy.RobocopySettings()).transfer()
